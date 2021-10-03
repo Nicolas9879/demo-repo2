80675a532b2aa9af54031a98941a1c0a1374486f
Nicolas9879 <87343173+Nicolas9879@users.noreply.github.com>
2021-10-03 11:50:42 -0300

    Create cash.c
    Prints the minimum number of coins with which that change can be made.

---
+++ cash.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+int main(void)
+{
+    // Ask for change owed
+    float dollars;
+    do
+    {
+        dollars = get_float("Change owed: ");
+    }
+    while (dollars < 0);
+
+    int change = round(dollars * 100);
+    int coins;
+
+    // quarter = 25, dime = 10, nickel = 5, penny = 1
+
+    for (coins = 0; change >= 25; coins++)
+    {
+        change = change - 25;
+    }
+     for (; change >= 10; coins++)
+    {
+        change = change - 10;
+    }
+    for (; change >= 5; coins++)
+    {
+        change = change - 5;
+    }
+    for (; change == 1; coins++)
+    {
+        change = change - 1;
+    }
+  
+    printf( "%i\n", coins);
+}
